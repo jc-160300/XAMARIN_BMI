b2e12f9af1ee56f483ccf02f9e61e5acfc8674d1
里紗 佐藤 <160300@jc-21.jp>
2018-11-22 12:46:55 +0900

    プロジェクト ファイルを追加します。

---
+++ XamarinBMI/XamarinBMI/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using BMI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinBMI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ButtonBMI.Clicked += ButtonBmiEvent;
+            ButtonBMI2.Clicked += ButtonClickEvent2;
+        }
+
+        private void ButtonClickEvent2(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            float weight = float.Parse(EntryWeight.Text);
+
+            if (height <= 105)
+            {
+                LabelResult.Text = "" + weight / (height * height);
+                float BMI = weight / (height * height);
+                Navigation.PushModalAsync(new NavigationPage(new BmiResult(BMI)));
+            }
+            else
+            {
+                height = height / 100;
+                LabelResult.Text = "" + weight / (height * height);
+                float BMI = weight / (height * height);
+                Navigation.PushModalAsync(new NavigationPage(new BmiResult(BMI)));
+            }
+
+
+
+
+        }
+        private void ButtonBmiEvent(object sender, EventArgs e)
+        {
+
+            float height = float.Parse(EntryHeight.Text);
+            float weight = float.Parse(EntryWeight.Text);
+
+            if (height <= 105)
+            {
+                LabelResult.Text = "" + weight / (height * height);
+            }
+            else
+            {
+                height = height / 100;
+                LabelResult.Text = "" + weight / (height * height);
+            }
+        }
+    }
+}
